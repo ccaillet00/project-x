9debd7d4570a22b3b63ceb86ea56479e5553d529
ccaillet00 <ccaillet@swissonline.ch>
2025-12-17 14:54:59 +0100

    added frontend folder

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,9 +2,11 @@ import type { Express } from "express"
 import { initializePostsAPI } from "./api"
 import { iniializeAuthAPI } from "./auth"
 import authMiddleware from "./auth-middlewares"
+import cors from "cors"
 
 export const initializeAPI = (app: Express) => {
     app.use("/api", authMiddleware)
+    app.use(cors())
     initializePostsAPI(app)
     iniializeAuthAPI(app)
 }
