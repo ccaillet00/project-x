4ab1c2a1dffb0dfec338bc6d83494170446e170a
ccaillet00 <ccaillet@swissonline.ch>
2026-03-15 22:20:10 +0100

    feature: added rate limiter

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -3,9 +3,11 @@ import { initializePostsAPI } from "./api";
 import { iniializeAuthAPI } from "./auth";
 import authMiddleware from "./auth-middlewares";
 import cors from "cors";
+import { limiter } from "./rate-limiter";
 
 export const initializeAPI = (app: Express) => {
   app.use("/api", authMiddleware);
+  app.use(limiter);
   app.use(cors());
   initializePostsAPI(app);
   iniializeAuthAPI(app);
